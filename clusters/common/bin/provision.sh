6e1945eca2181334d8a58482c9717abae6aa7f2c
ssenator <github@senator.net>
2020-05-21 00:29:59 -0600

    Issue #77 Prepopulate & allow for multiple user accounts

--- clusters/common/bin/provision.sh
+++ clusters/common/bin/provision.sh
@@ -591,11 +591,11 @@ CopyHomeVagrant() {
     if [ ! -d "${HOMEVAGRANT}" ] ; then
       Rc ErrExit ${EX_OSFILE} "mkdir -p ${HOMEVAGRANT}"
     fi
-    size=$(du -x -s -m ${VC}/* --exclude=common/repos --exclude='repos.tgz*' --exclude=repos | \
+    size=$(du -x -s -m ${VC}/* --exclude=common/repos --exclude='repos.tgz*' --exclude=repos --exclude='*.vdi' | \
 				awk 'BEGIN {total=0} {total += $1} END {print total}')
     Verbose " ${size}Mb "
     Rc ErrExit ${EX_SOFTWARE} "tar -cf - -C ${VC} \
-         --one-file-system --exclude='common/repos' --exclude='repos.tgz*' --exclude=repos --exclude='*.iso' --exclude='._*' . | \
+         --one-file-system --exclude='common/repos' --exclude='repos.tgz*' --exclude='*.vdi' --exclude=repos --exclude='*.iso' --exclude='._*' . | \
                               (cd ${HOMEVAGRANT}; tar ${TAR_LONG_ARGS} -${TAR_DEBUG_ARGS}${TAR_ARGS}f -)"
     Rc ErrExit ${EX_OSFILE} "touch ${HOMEVAGRANT}/HOME\ VAGRANT; chmod 0 ${HOMEVAGRANT}/HOME\ VAGRANT"
   fi
@@ -935,6 +935,16 @@ UserAdd() {
   local users_add=$(echo $(ls ${USERADD}))
   local u
 
+  if [ -f ${ETC_DEFAULT_USERADD} ] ; then
+    ## XXX sed-fu
+    sed -i~ -e "/HOME=\/home/d"  ${ETC_DEFAULT_USERADD}
+    sed -i~ -e "/INACTIVE=-1/i\
+HOME=${HOME_BASEDIR}" ${ETC_DEFAULT_USERADD}
+    if [ $? -ne ${EX_OK} ] ; then
+      ErrExit ${EX_SOFTWARE} "sed /INACTIVE=-1/i => HOME=${HOME_BASEDIR}"
+    fi
+  fi
+
   for u in ${users_add}
   do
     if [ -L ${USERADD}/${u} -o -f ${USERADD}/${u} ] ; then
@@ -946,8 +956,10 @@ UserAdd() {
     if [ ! -d ${USERADD}/${u} ] ; then
       ErrExit ${EX_CONFIG} "${USERADD}/${u} is not a directory"
     fi
+    if [ -f ${USERADD}/${u}/Template ] ; then
+      continue
+    fi
     cd ${USERADD}/${u} || ErrExit ${EX_OSERR} "cd ${USERADD}/${u}"
-    Verbose "  ${u}"
     local uid=""
     local gid=""
     local shell_arg=""
@@ -957,6 +969,18 @@ UserAdd() {
     local group_arg
     local dir_arg
     local exists
+    local multiple=""
+    local numeric="^[0-9]+$"
+
+    if [ -d multiple ] ; then
+      multiple=$(echo $(ls multiple))
+    fi
+    if [ -z "${multiple}" ] ; then
+      multiple=1
+    fi
+    if ! [[ ${multiple} =~ ${numeric} ]] ; then
+      ErrExit ${EX_CONFIG} "user: ${multiple}, non-numeric"
+    fi
 
     if [ ! -d uid ] ; then
       ErrExit ${EX_CONFIG} "user: ${u}, no uid"
@@ -967,134 +991,146 @@ UserAdd() {
     fi
     gid=$(echo $(ls gid))
 
-    if [ -d shell ] ; then
-      shell=$(ls shell)
-      shellpath=$(which $shell 2>&1)
-      if [ -x "${shellpath}" ] ; then
-        shell_arg="-s ${shellpath}"
+    for m in $(echo $(seq 1 ${multiple}))
+    do
+      local U=${u}
+      local _uid
+      local _gid
+      local msg
+
+      _uid=$(expr ${uid} + ${m} - 1)
+      _gid=$(expr ${gid} + ${m} - 1)
+
+      if [ "${multiple}" -eq 1 ] ; then
+        U=${u}
       else
-        Verbose "  Warning: ${shellpath} -- not executable"
+        U="${u}${m}"
       fi
-    fi
+      msg="${msg} ${U}: "
 
-    group_arg=""
-    if [ -d groups ] ; then
-      local ls_groups=$(echo $(ls groups))
-      groups=$(echo ${ls_groups} | sed 's/ /,/g')
-
-      if [ -n "${groups}" ] ; then 
-        group_arg="-G ${groups}"
-        Verbose "   + groups: ${groups}"
-      fi
-    fi
-
-    if [ -f ${ETC_DEFAULT_USERADD} ] ; then
-      ## XXX
-      sed -i~ -e "/HOME=\/home/d"  ${ETC_DEFAULT_USERADD}
-      sed -i~ -e "/INACTIVE=-1/i\
-HOME=${HOME_BASEDIR}" ${ETC_DEFAULT_USERADD}
-      if [ $? -ne ${EX_OK} ] ; then
-        ErrExit ${EX_SOFTWARE} "sed /INACTIVE=-1/i => HOME=${HOME_BASEDIR}"
-      fi
-    fi
-
-    dir_arg=""
-    dir=""
-    if [ -d ${HOME_BASEDIR} -o -d ${HOME_BASEDIR}/${u} ] ; then
-      if [ -d ${HOME_BASEDIR}/${u} ] ; then
-        dir_arg="-d ${HOME_BASEDIR}/${u}"
-        dir=${HOME_BASEDIR}/${u}
-      elif [ -d ${HOME_BASEDIR} ] ; then
-        dir_arg="-b ${HOME_BASEDIR}"
-        dir=${HOME_BASEDIR}/${u}
-      fi
-    fi
-
-    exists=$(echo $(getent passwd ${u} 2>&1))
-    if [ -z "${exists}" ] ; then
-      gid_explicit=""
-      if (( ${uid} != ${gid} )) ; then
-        group_arg="-G ${gid}"
-      else
-        gid_explicit="-U"
-      fi
-      Rc ErrExit ${EX_OSERR} "useradd -u ${uid} ${gid_explicit} -o ${shell_arg} ${group_arg} ${dir_arg} ${u}"
-    else
-      if [ -n "${shell_arg}" ] ; then
-        Rc ErrExit ${EX_OSERR} "chsh ${shell_arg} ${u}"
-      fi
-      if [ -n "${group_arg}" ] ; then
-        Rc ErrExit ${EX_OSERR} "usermod ${group_arg} ${u}"
-      fi
-      if [[ ${dir_arg} =~ -d ]] ; then
-        Rc ErrExit ${EX_OSERR} "usermod ${dir_arg} ${u}"
-      fi
-    fi
-
-    if [ -d "${USERADD_PASSWD}" ] ; then
-      if [ ! -f "${USERADD_PASSWD_CLEARTEXT}" -a ! -f "${USERADD_PASSWD_ENCRYPTED}" ] ; then
-        Verbose "   - passwd"
-        Rc ErrExit ${EX_OSERR} "passwd -d ${u} >/dev/null 2>&1"
-
-      elif [ -f "${USERADD_PASSWD_ENCRYPTED}" -a -s "${USERADD_PASSWD_ENCRYPTED}" ] ; then
-        local pw=$(echo $(cat ${USERADD_PASSWD_ENCRYPTED}))
-        Rc ErrExit ${EX_OSERR} "echo \"${u}:${pw}\" | chpasswd -e"
-
-      elif [ -f "${USERADD_PASSWD_CLEARTEXT}" -a -s "${USERADD_PASSWD_CLEARTEXT}" ] ; then
-        local pw=$(echo $(cat ${USERADD_PASSWD_CLEARTEXT}))
-        Verbose "   Note: setting cleartext passwd for user: ${u} (Ensure PermitEmptyPasswords is allowed in sshd_config.)"
-        Rc ErrExit ${EX_OSERR} "echo \"${u}:${pw}\" | chpasswd "
-
-      else
-        ErrExit ${EX_CONFIG} "broken password config: ${USERADD}/${u}/${USERADD_PASSWD}"
-      fi
-    fi
-
-    if [ -d ${USERADD}/${u}/secontext ] ; then
-      local u_secontext=$(echo $(ls ${USERADD}/${u}/secontext))
-      if [ -n "${u_secontext}" ] ; then
-        if [ -d ${dir} ] ; then
-          local fstyp=$(stat -f --format="%T" .)
-          case "${fstyp}" in
-          xfs|ext*|jfs|ffs|ufs|zfs)
-            Rc ErrExit ${EX_OSERR} "chcon -R ${u_secontext} ${dir}"
-            local u_setype=$(echo "${u_secontext}" | sed 's/:/ /g' | awk '{print $3}')
-            if [ -z "${u_setype}" ] ; then
-              ErrExit ${EX_CONFIG} "${u}: empty u_setype, u_secontext:${u_secontext}" 
-            fi
-            Rc ErrExit ${EX_OSERR} "semanage fcontext -a -t ${u_setype} ${dir}/\(/.*\)\? ;"
-            ;;
-          nfs)
-            # silently skip
-            ;;
-          *)
-            Verbose " unable to set secontext:${u_secontext}"
-            Verbose " on dir: ${dir}, which has a file system type,"
-            Verbose " fstype:${fstyp}  which does not implement secontext extended attributes."
-            ;;
-          esac
+      if [ -d shell ] ; then
+        shell=$(ls shell)
+        shellpath=$(which $shell 2>&1)
+        if [ -x "${shellpath}" ] ; then
+          shell_arg="-s ${shellpath}"
+        else
+          Verbose "  Warning: ${shellpath} -- not executable"
         fi
       fi
-    fi
 
-    if [ -d ${dir} ] ; then
-      if [ ! -L ${home}/${u} ] ; then
-        Rc ErrExit ${EX_OSFILE} "ln -f -s ${dir} /home/${u}"
-      fi
-      Rc ErrExit ${EX_OSFILE} "chown -h ${u} /home/${u} >/dev/null 2>&1"
-      Rc ErrExit ${EX_OSFILE} "chown -R ${u} ${dir}     >/dev/null 2>&1"
-    fi
+      group_arg=""
+      if [ -d groups ] ; then
+        local ls_groups=$(echo $(ls groups))
+        groups=$(echo ${ls_groups} | sed 's/ /,/g')
 
-    if [ ! -d "${ETC_SUDOERS_D}" ] ; then
-      ErrExit ${EX_OSFILE} "${ETC_SUDOERS_D}: not a directory or does not exist, ${u}"
-    fi
-    local u_sudoers_d=${USERADD}/${u}/${SUDOERS_D}
-    if [ -d "${u_sudoers_d}" ] ; then
-      if [ -f "${u_sudoers_d}/${u}" ] ; then
-        Rc ErrExit ${EX_OSFILE} "cp ${u_sudoers_d}/${u} ${ETC_SUDOERS_D}/${u}"
-        Verbose "   + sudo"
+        if [ -n "${groups}" ] ; then 
+          group_arg="-G ${groups}"
+          msg="${msg} groups:${groups}"
+        fi
       fi
-    fi
+
+      dir_arg=""
+      dir=""
+      if [ -d ${HOME_BASEDIR} -o -d ${HOME_BASEDIR}/${U} ] ; then
+        if [ -d ${HOME_BASEDIR}/${U} ] ; then
+          dir_arg="-d ${HOME_BASEDIR}/${U}"
+          dir=${HOME_BASEDIR}/${U}
+        elif [ -d ${HOME_BASEDIR} ] ; then
+          dir_arg="-b ${HOME_BASEDIR}"
+          dir=${HOME_BASEDIR}/${U}
+        fi
+      fi
+
+      exists=$(echo $(getent passwd ${U} 2>&1))
+      if [ -z "${exists}" ] ; then
+        gid_explicit=""
+        if (( ${uid} != ${gid} )) ; then
+          group_arg="-G ${_gid}"
+        else
+          gid_explicit="-U"
+        fi
+        Rc ErrExit ${EX_OSERR} "useradd -u ${_uid} ${gid_explicit} -o ${shell_arg} ${group_arg} ${dir_arg} ${U}"
+      else
+        if [ -n "${shell_arg}" ] ; then
+          Rc ErrExit ${EX_OSERR} "chsh ${shell_arg} ${U}"
+        fi
+        if [ -n "${group_arg}" ] ; then
+          Rc ErrExit ${EX_OSERR} "usermod ${group_arg} ${U}"
+        fi
+        if [[ ${dir_arg} =~ -d ]] ; then
+          Rc ErrExit ${EX_OSERR} "usermod ${dir_arg} ${U}"
+        fi
+      fi
+
+      if [ -d "${USERADD_PASSWD}" ] ; then
+        if [ ! -f "${USERADD_PASSWD_CLEARTEXT}" -a ! -f "${USERADD_PASSWD_ENCRYPTED}" ] ; then
+          msg="${msg} -passwd"
+          Rc ErrExit ${EX_OSERR} "passwd -d ${U} >/dev/null 2>&1"
+
+        elif [ -f "${USERADD_PASSWD_ENCRYPTED}" -a -s "${USERADD_PASSWD_ENCRYPTED}" ] ; then
+          local pw=$(echo $(cat ${USERADD_PASSWD_ENCRYPTED}))
+          Rc ErrExit ${EX_OSERR} "echo \"${U}:${pw}\" | chpasswd -e"
+
+        elif [ -f "${USERADD_PASSWD_CLEARTEXT}" -a -s "${USERADD_PASSWD_CLEARTEXT}" ] ; then
+          local pw=$(echo $(cat ${USERADD_PASSWD_CLEARTEXT}))
+          Verbose "   Note: setting cleartext passwd for user:${U} (Ensure PermitEmptyPasswords is allowed in sshd_config.)"
+          Rc ErrExit ${EX_OSERR} "echo \"${U}:${pw}\" | chpasswd "
+
+        else
+          ErrExit ${EX_CONFIG} "broken password config: ${USERADD}/${U}/${USERADD_PASSWD}"
+        fi
+      fi
+
+      if [ -d ${USERADD}/${u}/secontext ] ; then
+        local u_secontext=$(echo $(ls ${USERADD}/${u}/secontext))
+        if [ -n "${u_secontext}" ] ; then
+          if [ -d ${dir} ] ; then
+            local fstyp=$(stat -f --format="%T" .)
+            case "${fstyp}" in
+            xfs|ext*|jfs|ffs|ufs|zfs)
+              Rc ErrExit ${EX_OSERR} "chcon -R ${u_secontext} ${dir}"
+              local u_setype=$(echo "${u_secontext}" | sed 's/:/ /g' | awk '{print $3}')
+              if [ -z "${u_setype}" ] ; then
+                ErrExit ${EX_CONFIG} "${u}:empty u_setype, u_secontext:${u_secontext}" 
+              fi
+              Rc ErrExit ${EX_OSERR} "semanage fcontext -a -t ${u_setype} ${dir}/\(/.*\)\? ;"
+              ;;
+            nfs)
+              # silently skip
+              ;;
+            *)
+              Verbose " unable to set secontext:${u_secontext}"
+              Verbose " on dir: ${dir}, which has a file system type,"
+              Verbose " fstype:${fstyp}  which does not implement secontext extended attributes."
+              ;;
+            esac
+          fi
+        fi
+      fi
+
+      if [ -d ${dir} ] ; then
+        if [ ! -L ${home}/${U} ] ; then
+          Rc ErrExit ${EX_OSFILE} "ln -f -s ${dir} /home/${U}"
+        fi
+        Rc ErrExit ${EX_OSFILE} "chown -h ${U} /home/${U} >/dev/null 2>&1"
+        Rc ErrExit ${EX_OSFILE} "chown -R ${U} ${dir}     >/dev/null 2>&1"
+      fi
+
+      if [ ! -d "${ETC_SUDOERS_D}" ] ; then
+        ErrExit ${EX_OSFILE} "${ETC_SUDOERS_D}: not a directory or does not exist, ${u}"
+      fi
+      local u_sudoers_d=${USERADD}/${u}/${SUDOERS_D}
+      if [ -d "${u_sudoers_d}" ] ; then
+        if [ -f "${u_sudoers_d}/${u}" ] ; then
+          Rc ErrExit ${EX_OSFILE} "cp ${u_sudoers_d}/${u} ${ETC_SUDOERS_D}/${U}"
+          Rc ErrExit ${EX_OSFILE} "sed -i -e 's/${u}/${U}/' ${ETC_SUDOERS_D}/${U} ; "
+          msg="${msg} +sudo"
+        fi
+      fi
+      Verbose " ${msg}"
+      Verbose ""
+      msg=""
+    done
   done
 
   cd ${ORIGPWD} || ErrExit ${EX_OSERR} "cd ${ORIGPWD}"
