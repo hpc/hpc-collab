888e407e7db9e6ff245657395a830bd5c14236b1
ssenator <github@senator.net>
2020-07-09 00:29:57 -0600

    update changelog.h.sh to reflect git issues

--- clusters/common/inc/changelog.h.sh
+++ clusters/common/inc/changelog.h.sh
@@ -7,10 +7,11 @@
 ## @brief This header defines the changelog index for all of the libraries,
 ## functions, documentation and copackaged files.
 
-## initial preproduction baseline:		Dec 31 2019
-## multiple headers and libraries		Jan 30 2020
-## multiple clusters (bulk copy)		Feb 28 2020
-## user verification, slurm version		Mar 30 2020
-## multiple clusters dependency 		Apr 15 2020
-## multiple clusters cfg tree merge 		Apr 30 2020
+## initial preproduction baseline:				Dec 31 2019
+## multiple headers and libraries				Jan 30 2020
+## multiple clusters (bulk copy)				Feb 28 2020
+## user verification, slurm version				Mar 30 2020
+## multiple clusters dependency 				Apr 15 2020
+## multiple clusters cfg tree merge 				Apr 30 2020
+## >>> transition to https://github.com/hpc/hpc-collab/issues
 
