43e64fddbfa4033dc3d1f3e657147237dd4b8e83
ssenator <github@senator.net>
2020-07-13 02:03:25 -0600

    inconsistent flags (NO_NFS, SKIP_SW:vboxadd) don't cause ErrExit, only Warnings

--- clusters/common/lib/setflags.sh
+++ clusters/common/lib/setflags.sh
@@ -131,17 +131,15 @@ SetFlags() {
     SKIP_SW)
         export SKIP_SW=$(cat ${FLAGS}/SKIP_SW)
         set_flags="${set_flags} SKIP_SW:\"${SKIP_SW}\""
-
 	if [ -n "${NO_NFS}" ] ; then
 		if [[ "${SKIP_SW}" != *vboxadd* ]] ; then
-			( Warn ${EX_CONFIG} "  Virtualbox shared folders (vboxsf) are enabled (not skipped)"
-			  Warn ${EX_CONFIG} "  but NO_NFS is set, also."
-			  Warn ${EX_CONFIG} "  Provisioning would halt with no source upon upgrading the"
-			  Warn ${EX_CONFIG} "  the virtualbox guest additions."
-			  Warn ${EX_CONFIG} "  To remediate: set clusters/common/flag/SKIP_SW to 'vboxadd'"
-			  Warn ${EX_CONFIG} "  To remediate:     or remove NO_NFS flag"
-			) > /dev/tty
-			ErrExit ${EX_CONFIG} "SKIP_SW:${SKIP_SW} && NO_NFS"
+			Warn ${EX_CONFIG} "  Virtualbox shared folders (vboxsf) are enabled (not skipped)"
+			Warn ${EX_CONFIG} "  but NO_NFS is set, also."
+			Warn ${EX_CONFIG} "  Provisioning would halt with no source upon upgrading the"
+			Warn ${EX_CONFIG} "  the virtualbox guest additions."
+			Warn ${EX_CONFIG} "  "
+			Warn ${EX_CONFIG} "  To remediate: set clusters/common/flag/SKIP_SW to 'vboxadd'"
+			Warn ${EX_CONFIG} "  To remediate:     or remove NO_NFS flag"
 		fi
 	fi
         ;;
@@ -178,15 +176,13 @@ SetFlags() {
           export NO_NFS="NO_NFS"
           set_flags="${set_flags} NO_NFS"
 	fi
-	if [[ "${SKIP_SW}" = *vboxadd* ]] ; then
-		( Warn ${EX_CONFIG} "  Virtualbox shared folders (vboxsf) are enabled (not skipped)"
-		  Warn ${EX_CONFIG} "  but NO_NFS is set, also."
-		  Warn ${EX_CONFIG} "  Provisioning would halt with no source upon upgrading the"
-		  Warn ${EX_CONFIG} "  the virtualbox guest additions."
-		  Warn ${EX_CONFIG} "  To remediate: set clusters/common/flag/SKIP_SW to 'vboxadd'"
-		  Warn ${EX_CONFIG} "  To remediate:     or remove NO_NFS flag"
-		) > /dev/tty
-		ErrExit ${EX_CONFIG} "SKIP_SW:${SKIP_SW} && NO_NFS"
+	if [[ "${SKIP_SW}" != *vboxadd* ]] ; then
+		Warn ${EX_CONFIG} "  Virtualbox shared folders (vboxsf) are enabled (not skipped) but NO_NFS is set, also."
+		Warn ${EX_CONFIG} "  Provisioning would halt with no source upon upgrading the"
+		Warn ${EX_CONFIG} "  virtualbox guest additions."
+		Warn ${EX_CONFIG} "  "
+		Warn ${EX_CONFIG} "  To remediate: "
+		Warn ${EX_CONFIG} "      set clusters/common/flag/SKIP_SW to 'vboxadd' or remove NO_NFS flag"
 	fi
 	;;
     esac
