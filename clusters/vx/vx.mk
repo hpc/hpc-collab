9f2eef934af5ee61602b7663194b5ad5c29d58dc
ssenator <github@senator.net>
2020-08-02 20:54:25 -0600

    enforce regeneration if predecessor cluster appears to have been changed

--- clusters/vx/vx.mk
+++ clusters/vx/vx.mk
@@ -6,7 +6,16 @@
 ### ### ### ###
 
 # used to search for sources for tarballs, etc
-ALTERNATE_VC_D	= ../vc~
+PREREQ_VC_D	= ../vc
+ALTERNATE_VC_D	= $(PREREQ_VC_D)~
+
+PREREQ_COMMON		= $(PREREQ_VC_D)/common
+PREREQ_COMMONETC	= $(PREREQ_COMMON)/etc
+PREREQ_HOSTS		= $(PREREQ_COMMONETC)/hosts
+
+COMMON			= common
+COMMON_ETC		= $(COMMON)/etc
+HOSTS			= $(COMMON_ETC)/hosts
 
 $(SAVELOGS_TARGETS): $(wildcard $(PROVISIONED_D)/*)
 
@@ -17,9 +26,19 @@ $(SAVELOGS_TARGETS): $(wildcard $(PROVISIONED_D)/*)
 # at present the following assumes that /etc/hosts has target cluster nodes set up
 #
 # @todo XXX tear down the cluster data base in our required external db cluster when we are unprovisioned
+#
+#
 
+# vx depends on its conjoined cluster vc for /etc/hosts, so IP addresses in Vagrantfile and elsewhere
+$(HOSTS): $(PREREQ_HOSTS)
+	$(HUSH)rsync -LHcau $< $@
+	$(HUSH)env VC=$(IAM) ../common/bin/generateproviderfiles.sh
+	$(HUSH)env VC=$(IAM) ../common/bin/compilevagrantfile.sh
 
 $(STATE_DIRS_ALL):
 	$(HUSH)mkdir -p $@
 
 ### ### ### ###
+
+# vim: background=dark
+
