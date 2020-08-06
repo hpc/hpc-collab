ac7e49214cc3ebe5183fa1d8e42e3ef3e819a21c
ssenator <github@senator.net>
2020-08-06 14:53:10 -0600

    add dependency for generating provider files to in-cluster hosts file

--- clusters/vc/vc.mk
+++ clusters/vc/vc.mk
@@ -3,6 +3,9 @@ ALTERNATE_VC_D	= ../vc~
 $(SAVELOGS_TARGETS): $(wildcard $(PROVISIONED_D)/*)
 	$(HUSH)env VC=$(VC) $(SAVE_LOGSDB)
 
+common/etc/hosts:
+	cd $(CLUSTERS_DIR)/$(VC) ; env VC=$(VC) $(GENERATE_PROVIDER_FILES)
+
 $(STATE_DIRS_ALL):
 	$(HUSH)mkdir -p $@
 
