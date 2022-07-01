ae0aa26c62a5fe18933ccb625a928dc6a7497b33
Steven Senator <sts@turq-slurmdbta.lanl.gov>
2022-07-01 12:25:58 -0600

    db external to cluster

--- clusters/common/env/site-vars.sh
+++ clusters/common/env/site-vars.sh
@@ -3,3 +3,11 @@ export LANGUAGE=${LANGUAGE:-${LANG}}
 export LC_ALL=${LC_ALL:-${LANG}}
 
 export TZ=${TZ:-MST7MDT}
+
+export LANL_PROTO=http
+export LANL_PROXY_HOST=proxyout.lanl.gov
+export LANL_PROXY_PORT=8080
+export LANL_PROXY=${LANL_PROTO}://${LANL_PROXY_HOST}:${LANL_PROXY_PORT}
+export http_proxy=${LANL_PROXY}
+export https_proxy=${LANL_PROXY}
+export proxy=${LANL_PROXY}
